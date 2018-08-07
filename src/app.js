8a52dbd0d9de2f841264018a20bf84d6eb98090d
Gary Muir <garymuir17@gmail.com>
2018-08-07 17:38:29 +0100

    adds bindEvents to select_view

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+const instrumentsData = require("./data/instrument_families.js");
+const SelectView = require("./views/select_view.js");
+
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript Loaded');
+
+  const selectElement = document.querySelector("#instrument-families");
+  const selectView = new SelectView(selectElement);
+  selectView.bindEvents();
+
 });
